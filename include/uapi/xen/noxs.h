5d07f3587499c900aebb88309a9b41b3721d39ea
Costin Lupu <costin.lup@gmail.com>
2016-09-30 20:28:23 +0300

    noxenstore - updated interfaces

--- include/uapi/xen/noxs.h
+++ include/uapi/xen/noxs.h
@@ -8,40 +8,47 @@
 #ifndef INCLUDE_UAPI_XEN_NOXS_H_
 #define INCLUDE_UAPI_XEN_NOXS_H_
 
-#include <linux/types.h>
-#include <linux/compiler.h>
+//#include <linux/types.h>
+//#include <linux/compiler.h>
 
-#if 0
-#define _IOC(dir,type,nr,size) \
-	(((dir)  << _IOC_DIRSHIFT) | \
-	 ((type) << _IOC_TYPESHIFT) | \
-	 ((nr)   << _IOC_NRSHIFT) | \
-	 ((size) << _IOC_SIZESHIFT))
-#endif
+#include <xen/interface/noxs.h>
 
-enum noxs_dev_type {
-	noxs_dev_vif,
-};
 
-struct noxenbus_dev_req {
-	enum noxs_dev_type type;
-};
 
+typedef unsigned int evtchn_port_t;//TODO remove
+
+/*
 struct noxs_devpage_reg {
 	unsigned int domid;
 	evtchn_port_t port;
 	unsigned int mfn;
 };
+*/
 
-#define IOCTL_NOXS_DEVPAGE_REG					\
-	_IOC(_IOC_NONE, 'P', 0, sizeof(struct noxs_devpage_reg))
-#define IOCTL_NOXS_DEVREQ					\
-	_IOC(_IOC_NONE, 'P', 1, sizeof(struct noxenbus_dev_req))
-#define IOCTL_NOXENBUS_MMAP					\
-	_IOC(_IOC_NONE, 'P', 2, sizeof(struct privcmd_mmap))
-#define IOCTL_NOXENBUS_MMAPBATCH					\
-	_IOC(_IOC_NONE, 'P', 3, sizeof(struct privcmd_mmapbatch))
-#define IOCTL_NOXENBUS_MMAPBATCH_V2				\
-	_IOC(_IOC_NONE, 'P', 4, sizeof(struct privcmd_mmapbatch_v2))
+struct noxs_user_vif {
+	/* IN */
+	__u8 mac[ETH_ALEN];
+	__be32 ip;
+	/* OUT */
+};
+
+struct noxs_ioctl_devreq {
+	/* IN */
+	enum noxs_dev_type type;
+	unsigned int domid;
+	unsigned int devid; /* WIP */
+
+	union { /* device specific */
+		struct noxs_user_vif vif;
+	} dev;
+
+	/* OUT */
+	unsigned int mfn;//TODO rename
+	evtchn_port_t evtchn;
+};
+
+
+#define IOCTL_NOXS_DEVREQ \
+	_IOC(_IOC_NONE, 'P', 1, sizeof(struct noxs_ioctl_devreq))
 
 #endif /* INCLUDE_UAPI_XEN_NOXS_H_ */
--- include/xen/interface/noxs.h
+++ include/xen/interface/noxs.h
@@ -8,56 +8,96 @@
 #ifndef XEN_PUBLIC_IO_NOXS_H_
 #define XEN_PUBLIC_IO_NOXS_H_
 
-#include "../event_channel.h"
+#include "event_channel.h"
 #include "../grant_table.h"
-#include "xenbus.h"
+#include "io/xenbus.h"
 
 
 #define NOXS_DEV_COUNT_MAX 32/*TODO this is defined at hyp level*/
 
 
 enum noxs_dev_type {
-    noxs_dev_none = 0,
-    noxs_dev_console,
-    noxs_dev_vif,
+	noxs_dev_none = 0,
+	noxs_dev_console,
+	noxs_dev_vif,
 };
 
-struct noxs_dev_info {
-    enum noxs_dev_type type;
+enum noxs_watch_state {
+	noxs_watch_none = 0,
+	noxs_watch_requested,
+	noxs_watch_updated
+};
 
-    domid_t be_id;
-    uint32_t be_state;
-    uint32_t fe_state;
+struct noxs_dev_page_entry {
+	enum noxs_dev_type type;
 
-    unsigned int mfn;
-    evtchn_port_t port;
+	domid_t be_id;
+
+	unsigned int mfn;
+	evtchn_port_t port;
 };
 
 struct noxs_dev_page {
-    uint32_t version;
-    uint32_t dev_count;
-    struct noxs_dev_info devs[NOXS_DEV_COUNT_MAX];
+	uint32_t version;
+	uint32_t dev_count;
+	struct noxs_dev_page_entry devs[NOXS_DEV_COUNT_MAX];
 };
 
 struct noxs_dev_entry {
-    unsigned int idx;
-    struct noxs_dev_info *dev_info;
+	unsigned int idx;
+	struct noxs_dev_page_entry *dev_info;
 };
 
-struct noxs_dev_vif {
-    uint32_t version;
+struct noxs_ctrl_hdr {
+	uint16_t domid;
+	int devid;
+	int grant;
+	int evtchn;
+	int be_state;
+	int fe_state;
 
-    char mac[18];
-
-    evtchn_port_t port;
-    unsigned int tx_ring_ref;
-    unsigned int rx_ring_ref;
-
-    struct {
-        uint32_t sg;
-        uint32_t gso_ipv4;
-        uint32_t gso_ipv6;
-    } features;
+	enum noxs_watch_state fe_watch_state;
+	enum noxs_watch_state be_watch_state;
 };
+
+struct vif_features {
+	__u8 rx_notify :1;
+	__u8 sg :1;
+	__u8 gso_tcpv4 :1;
+	__u8 gso_tcpv4_prefix :1;
+	__u8 gso_tcpv6 :1;
+	__u8 gso_tcpv6_prefix :1;
+	__u8 no_csum_offload :1;
+	__u8 ipv6_csum_offload :1;
+	__u8 rx_copy :1;
+	__u8 rx_flip :1;
+	__u8 multicast_control :1;
+	__u8 dynamic_multicast_control :1;
+	__u8 split_event_channels :1;
+	__u8 ctrl_ring :1;
+};
+
+#define ETH_ALEN    6       /* Octets in one ethernet addr   */
+
+struct noxs_vif_ctrl_page {
+	struct noxs_ctrl_hdr hdr;
+	__u8 mac[ETH_ALEN];
+	__be32 ip;
+	int vifid;
+	struct vif_features feature;
+	int multi_queue_max_queues;
+	int multi_queue_num_queues;
+
+	unsigned int tx_ring_ref;
+	unsigned int rx_ring_ref;
+	unsigned int event_channel_tx;
+	unsigned int event_channel_rx;
+	unsigned int request_rx_copy;
+
+	int ctrl_ring; //bool
+	unsigned int ctrl_ring_ref;
+	int event_channel_ctrl;
+};
+
 
 #endif /* XEN_PUBLIC_IO_NOXS_H_ */
