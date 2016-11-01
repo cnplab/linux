11ac7e8bdd9000a4043f47f9dc597e1cfae5633e
Costin Lupu <costin.lup@gmail.com>
2016-11-01 17:00:20 +0200

    noxenstore - cleanup

--- include/xen/noxs.h
+++ include/xen/noxs.h
@@ -205,7 +205,6 @@ struct xenbus_driver {
 	int (*read_otherend_details)(struct xenbus_device *dev);
 #endif
 	int (*is_ready)(struct xenbus_device *dev);
-	int (*set_user_data)(struct xenbus_device *dev);
 };
 
 static inline struct xenbus_driver *to_xenbus_driver(struct device_driver *drv)
@@ -227,42 +226,6 @@ int __must_check __noxs_register_backend(struct xenbus_driver *drv,
 
 void xenbus_unregister_driver(struct xenbus_driver *drv);
 
-struct xenbus_transaction
-{
-	u32 id;
-};
-
-/* Nil transaction ID. */
-#define XBT_NIL ((struct xenbus_transaction) { 0 })
-
-char **xenbus2_directory(struct xenbus_transaction t,
-			const char *dir, const char *node, unsigned int *num);
-void *xenbus2_read(struct xenbus_transaction t,
-		  const char *dir, const char *node, unsigned int *len);
-int xenbus2_write(struct xenbus_transaction t,
-		 const char *dir, const char *node, const char *string);
-int xenbus2_mkdir(struct xenbus_transaction t,
-		 const char *dir, const char *node);
-int xenbus2_exists(struct xenbus_transaction t,
-		  const char *dir, const char *node);
-int xenbus2_rm(struct xenbus_transaction t, const char *dir, const char *node);
-int xenbus2_transaction_start(struct xenbus_transaction *t);
-int xenbus2_transaction_end(struct xenbus_transaction t, int abort);
-
-/* Single read and scanf: returns -errno or num scanned if > 0. */
-__scanf(4, 5)
-int xenbus2_scanf(struct xenbus_transaction t,
-		 const char *dir, const char *node, const char *fmt, ...);
-
-/* Single printf and write: returns -errno or 0. */
-__printf(4, 5)
-int xenbus2_printf(struct xenbus_transaction t,
-		  const char *dir, const char *node, const char *fmt, ...);
-
-/* Generic read function: NULL-terminated triples of name,
- * sprintf-style type string, and pointer. Returns 0 or errno.*/
-int xenbus2_gather(struct xenbus_transaction t, const char *dir, ...);
-
 /* notifer routines for when the xenstore comes up */
 extern int xenstored_ready;
 int register_xenstore_notifier(struct notifier_block *nb);
@@ -275,37 +238,10 @@ void xs_suspend(void);
 void xs_resume(void);
 void xs_suspend_cancel(void);
 
-/* Used by xenbus2_dev to borrow kernel's store connection. */
-void *xenbus2_dev_request_and_reply(struct xsd_sockmsg *msg);
-
 struct work_struct;
 
-/* Prepare for domain suspend: then resume or cancel the suspend. */
-void xenbus2_suspend(void);
-void xenbus2_resume(void);
 void noxs_probe(struct work_struct *);
-void xenbus2_suspend_cancel(void);
-
-#define XENBUS_IS_ERR_READ(str) ({			\
-	if (!IS_ERR(str) && strlen(str) == 0) {		\
-		kfree(str);				\
-		str = ERR_PTR(-ERANGE);			\
-	}						\
-	IS_ERR(str);					\
-})
-
-#define XENBUS_EXIST_ERR(err) ((err) == -ENOENT || (err) == -ERANGE)
-
-int xenbus_watch_path(struct xenbus_device *dev, const char *path,
-		      struct xenbus_watch *watch,
-		      void (*callback)(struct xenbus_watch *,
-				       const char **, unsigned int));
-__printf(4, 5)
-int xenbus_watch_pathfmt(struct xenbus_device *dev, struct xenbus_watch *watch,
-			 void (*callback)(struct xenbus_watch *,
-					  const char **, unsigned int),
-			 const char *pathfmt, ...);
-
+void noxs_notify_otherend(struct xenbus_device *dev);
 int xenbus_switch_state(struct xenbus_device *dev, enum xenbus_state new_state);
 int xenbus_grant_ring(struct xenbus_device *dev, void *vaddr,
 		      unsigned int nr_pages, grant_ref_t *grefs);
